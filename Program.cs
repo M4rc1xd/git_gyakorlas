7d262d00a06b8ab337e2dcad63989fbeb179d4c9
Kover Marcell <1228marci@gmail.com>
2024-12-16 17:28:37 +0100

    F01 feladat elkészítve

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace _12._13
+{
+    internal class Program
+    {
+        static Random r = new Random();
+        static void f01(int[] szamok)
+        {
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = r.Next(1, 51);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
